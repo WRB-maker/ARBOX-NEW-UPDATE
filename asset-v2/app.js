fabcc0e5137b4ca8bd0bb6d33783abddfda60f14
WRB-maker <kaibolden16@icloud.com>
2026-06-21 14:10:59 -0400

    Fix code sequence for Ravage entry

--- asset-v2/app.js
+++ asset-v2/app.js
@@ -89,7 +89,7 @@ var app = new function() {
     }, {
         name: "Ravage",
         src: "v2-b2-ravage-hb.mp4",
-        code: "2,5,8,9,13",
+        code: "2,4,8,11,13",
         sound: "22_bonus_ravage",
         aspire: "aspire"
     }, {
