c9c586c9a450e648f71caa80fc7c5cc00a0f6146
WRB-maker <kaibolden16@icloud.com>
2026-07-23 12:04:07 -0400

    Update app.js

--- asset-v5/app.js
+++ asset-v5/app.js
@@ -1,2 +1,3 @@
 /* 0.5.0 2026-07-23 00:00:00 */
-var app = new function() {
+var app = new function() 
+{this.name = "Indust", this.version = "5", this.date = "2026", this.folder = "asset-v5/", this.looptime = 9600, this.bpm = 100, this.totalframe = 488, this.nbpolo = 7, this.nbloopbonus = 8, this.bonusloopA = !0, this.bonusendloopA = !4, this.recmaxloop = 34, this.recminloop = 4, this.recmintime = Math.round(this.looptime / 1e3) * this.recminloop, this.spritepolo = "polo-sprite.png", this.spritepicto = "game-picto.png", this.colBck = "#1A1A1A", this.col0 = "#4A4A4A", this.col1 = "#383838", this.col2 = "#2E2E2E", this.col3 = "#242424", this.col4 = "#1F1F1F", this.animearray = [{
