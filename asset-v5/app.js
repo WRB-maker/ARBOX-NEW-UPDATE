555e263d6d09199a93b302b33e843fc2feb47eaf
WRB-maker <kaibolden16@icloud.com>
2026-07-27 14:59:22 -0400

    Update app.js

--- asset-v5/app.js
+++ asset-v5/app.js
@@ -2,3 +2,4 @@
 var app = new function() 
 {this.name = "labix", this.version = "5", this.date = "2026", this.folder = "asset-v5/", this.looptime = 8000, this.bpm = 120, this.totalframe = 488, this.nbpolo = 7, this.nbloopbonus = 8, this.bonusloopA = !0, this.bonusendloopA = !4, this.recmaxloop = 34, this.recminloop = 4, this.recmintime = Math.round(this.looptime / 1e3) * this.recminloop, this.spritepolo = "polo-sprite.png", this.spritepicto = "game-picto.png", this.colBck = "#1A1A1A", this.col0 = "#4A4A4A", this.col1 = "#383838", this.col2 = "#2E2E2E", this.col3 = "#242424", this.col4 = "#1F1F1F", this.animearray = [{
        name: "1_officer",
+       color:
